de418b3b2b6e56bb22ae705602ba17c0b6cb7245
juyeon Bae <w2316@e-mirim.hs.kr>
2025-03-27 19:10:02 +0900

    feat : not-found page 구현

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,14 @@
+import Image from "next/image"
+import Logo from "../../public/images/404Icon.png"
+import styles from "../app/styles/not-found.module.css"
 export default function NotFound() {
     return (
-        <h1>Not Found</h1>
+        <div className={styles.messageContainer}>
+            <Image src={Logo} alt="logo" width={370} height={262} />
+            <div style={{ textAlign: "center" }}>
+                <p className={styles.message}>404</p>
+                <p className={styles.message2}>Not found Page</p>
+            </div>
+        </div >
     )
 }
