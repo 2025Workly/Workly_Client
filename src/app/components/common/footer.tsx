4c374cb00997bf161f57886b14b0023208ffbf81
juyeonBae <w2316@e-mirim.hs.kr>
2025-04-13 00:59:45 +0900

    feat : 메인 페이지일때만 footer style 다르게 수정

--- src/app/components/common/footer.tsx
+++ src/app/components/common/footer.tsx
@@ -2,24 +2,29 @@ import styles from "../../styles/footer.module.css"
 import Link from "next/link"
 import Image from "next/image"
 import Logo from "../../../../public/images/testLogo.png"
-export default function Footer() {
+
+interface FooterProps {
+    isHome: boolean;
+}
+export default function Footer({ isHome }: FooterProps) {
     return (
 
-        <footer className={styles.footer}>
-            <Image src={Logo} alt="logo" className={styles.LogoImg} />
+        <footer className={styles.footer} style={{ background: isHome ? "linear-gradient(90deg, #3B44E6 0.01%, #396AE0 100%)" : "#F7F7F7" }}>
+            <div style={{ display: "flex", alignItems: "center" }}>
+                <Image src={Logo} alt="logo" className={styles.LogoImg} />
+                <div className={styles.mainFooter}>
+                    <div className={styles.producer}>
+                        <span style={{ color: isHome ? "#AFAFAF" : "#727272" }}>제작자</span>
+                        <span className={styles.member} style={{ color: isHome ? "#A4A4A4" : "#6F6F6F" }}>배주연, 이성미, 박수민, 양현서</span>
+                    </div>
 
-            <div className={styles.mainFooter}>
-                <div className={styles.producer}>
-                    <span>제작자</span>
-                    <span className={styles.member}>배주연, 이성미, 박수민, 양현서</span>
+                    <ul className={styles.ul}>
+                        <li><Link href={"/calender"} style={{ color: isHome ? "#BBB" : "#6F6F6F" }}>일정관리</Link></li>
+                        <li><Link href={"/notice-board"} style={{ color: isHome ? "#BBB" : "#6F6F6F" }}>게시판</Link></li>
+                        <li><Link href={"/job-tips-words"} style={{ color: isHome ? "#BBB" : "#6F6F6F" }}>직무</Link></li>
+                        <li><Link href={"/mypage"} style={{ color: isHome ? "#BBB" : "#6F6F6F" }}>마이페이지</Link></li>
+                    </ul>
                 </div>
-
-                <ul className={styles.ul}>
-                    <li><Link href={"/calender"}>일정관리</Link></li>
-                    <li><Link href={"/notice-board"}>게시판</Link></li>
-                    <li><Link href={"/job-tips-words"}>직무</Link></li>
-                    <li><Link href={"/mypage"}>마이페이지</Link></li>
-                </ul>
             </div>
         </footer>
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+
 import type { Metadata } from "next";
-import "../app/styles/globals.css"
+import "../app/styles/globals.css";
 import Nav from "./components/common/nav-bar";
 import Footer from "./components/common/footer";
+import FooterWrapper from "./components/common/footer-wrapper";
 export const metadata: Metadata = {
   title: {
     template: "Workly",
@@ -9,18 +11,19 @@ export const metadata: Metadata = {
   },
   description: "Website for young professionals",
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
-
       <body>
         <Nav />
         {children}
-        <Footer />
+        <FooterWrapper />
       </body>
     </html>
   );
