f7561a8c2166d4cf741802ba5fa3256f965cc658
juyeonBae <w2316@e-mirim.hs.kr>
2025-05-11 04:05:30 +0900

    feat : 단어 목록 불러오는 api 연결

--- src/app/(job-tips-words)/[id]/page.tsx
+++ src/app/(job-tips-words)/[id]/page.tsx
@@ -3,42 +3,101 @@ import Category from "../../components/common/category-layout";
 import SearchBar from "@/app/components/WordTip/search-bar-layout";
 import Button from "@/app/components/common/button-layout";
 import styles from "../../../app/(job-tips-words)/job-layout.module.css"
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Link from "next/link";
 import Write from "@/app/components/WordTip/wirte-content-popup";
 import InfoCard from "@/app/components/WordTip/infoCard-layout";
 import Header from "@/app/components/WordTip/header-layout";
 
+
+
 export default function Job() {
     const [activeCategory, setActiveCategory] = useState<string>("전체")
+    const [showWritePopup, setShowWritePopup] = useState(false)
+    const [words, setWords] = useState<any[]>([]) //단어 data 저장할 상태
+
     const categories = ["전체", "개발", "디자인"];
-    const handleCategoryClick = (category: string) => {
+    const token = process.env.NEXT_PUBLIC_API_TOKEN;
+
+    const handleCategoryClick = async (category: string) => {
         setActiveCategory(category)
+
+        try {
+            const response = await axios.get(`http://localhost:3000/words/${category}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            setWords(response.data.words)
+        } catch (err) {
+            console.error('api호출 오류 : ', err)
+        }
     }
+
+    useEffect(() => {
+        handleCategoryClick('전체')
+    }, [])
+
 
     return (
         <div className={styles.allContainer}>
-            <Header />
-            <SearchBar title={"단어"} />
-            <div className={styles.categoryContainer}>
-                <div className={styles.categoryList}>
-                    {categories.map((category) => (
-                        <Category
-                            key={category}
-                            type={category}
-                            onClick={() => handleCategoryClick(category)}
-                            isActive={category === activeCategory}
-                        />
-                    ))}
+            <div style={{ zIndex: "-100" }}>
+                <div className={styles.searchHeaderContainer}>
+                    <Header />
+                    <SearchBar title={"단어"} />
                 </div>
-                <span className={styles.addWordsBtn}><Link href="">단어추가 +</Link></span>
+
+
+                {/* 카테고리 / 단어 추가 btn */}
+                <div className={styles.categoryContainer}>
+                    <div className={styles.categoryList}>
+                        {categories.map((category) => (
+                            <Category
+                                key={category}
+                                type={category}
+                                onClick={() => handleCategoryClick(category)}
+                                isActive={category === activeCategory}
+                            />
+                        ))}
+                    </div>
+
+                    <span className={styles.addWordsBtn}
+                        onClick={() => setShowWritePopup(true)}>
+                        단어추가 +
+                    </span>
+                </div>
+
+                {/* 정보 카드들 */}
+                <div className={styles.infoCardContainer}>
+                    {words.length > 0 ? (
+                        words.map((word) => (
+                            <InfoCard
+                                key={word.id}
+                                title={word.word}
+                                detail={word.explanation}
+                                width="344px"
+                                padding="39px 52px"
+                                gap="98px"
+                            />
+                        ))
+                    ) : (<p>해당 카테고리에 해당하는 직무 팁이 없습니다.</p>)}
+                </div>
+
             </div>
-            <InfoCard
-                width="527px" padding="43px 57px" gap="98px"
-                title="일러스트 CMYK에서 RGB로 변경하기"
-                detail="벡터 이미지는 수학적으로 계산된 경로로 구성된 무한 확대 가능한 디지털 그래픽입니다. 벡터는 기본적으로 필요에 의해 늘어나거나 구부러질 수 있습니다."
-            />
-            {/* <Write mainPlaceholder="단어를" buttonTitle="단어 게시하기" contentPlaceholder="단어의 뜻을" /> */}
-        </div>
+            {/* 작성 팝업 */}
+            <div style={{ zIndex: "1000" }}>
+                {showWritePopup && (
+                    <Write
+                        mainPlaceholder="단어를"
+                        contentPlaceholder="단어의 뜻을"
+                        buttonTitle="팁 게시하기"
+                        closeOnClick={() => setShowWritePopup(false)}
+                    />
+
+                )}
+            </div>
+
+        </div >
     )
 }
