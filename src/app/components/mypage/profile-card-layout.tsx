1b5fb21e19dfe9d1ea937a6e4ab1bda70d9a2516
juyeonBae <w2316@e-mirim.hs.kr>
2025-05-11 06:21:10 +0900

    feat : profile-card component

---
+++ src/app/components/mypage/profile-card-layout.tsx
@@ -0,0 +1,28 @@
+"use client"
+import styles from "../../styles/mypage/profile-card.module.css"
+import Modal from "./modal-layout"
+import { useState } from "react"
+
+interface ProfileContentProps {
+    username: string,
+    userId: string,
+}
+
+export default function ProfileCard({ username, userId }: ProfileContentProps) {
+    const [open, setOpen] = useState(false)
+
+    return (
+        <div className={styles.profileCardContainer}>
+            <div className={styles.contentContainer}>
+                <img src="/images/profileImg.png" className={styles.profileImg} />
+                <p className={styles.username}>{username}</p>
+                <p className={styles.userId}>{userId}</p>
+            </div>
+
+            <img src="/images/modal.png"
+                onClick={() => setOpen(!open)}
+                style={{ width: "6px", height: "33px", cursor: "pointer" }} />
+            {open ? <Modal /> : null}
+        </div>
+    )
+}
