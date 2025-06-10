292dbe1172f5d3363808d497dbacf770de8acda0
juyeonBae <w2316@e-mirim.hs.kr>
2025-06-11 00:36:55 +0900

    feat : 체크 리스트 추가 화면만 구현

--- src/app/calender/page.tsx
+++ src/app/calender/page.tsx
@@ -1,12 +1,17 @@
 'use client'
 import styles from "../styles/Calender/calender-main.module.css"
+import Calendar from "../components/calender/calender-layout"
+import CheckList from "../components/calender/check-list-layout"
+
 export default function Calender() {
     return (
         <div className={styles.allContainer}>
             <div style={{ width: "1070px", margin: "auto" }}>
                 <h2 className={styles.h2}>야근 및 일정관리</h2>
-
-
+                <div style={{ display: "flex", gap: "24px" }}>
+                    <Calendar />
+                    <CheckList />
+                </div>
             </div>
 
         </div >
