45ae97daf3fb6a1511c25944516631ede3995eec
juyeonBae <w2316@e-mirim.hs.kr>
2025-06-16 04:07:35 +0900

    chore : file 정리

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/mypage/mypage.module.css";
 import ProfileCard from "../components/mypage/profile-card-layout";
 import Calculator from "../components/mypage/calculator-layout";
 export default function Mypage() {
-    const [token, setToken] = useState<string | null>(null);
+  const [token, setToken] = useState<string | null>(null);
   const [username, setUsername] = useState<string | null>(null);
   const [email, setEmail] = useState<string | null>(null);
 
@@ -19,22 +19,21 @@ export default function Mypage() {
     setEmail(storedEmail);
   }, []);
 
+  return (
+    <div className={styles.allContainer}>
+      <div className={styles.profileContainer}>
+        <p className={`${styles.contentName} ${styles.mypageTitle}`}>
+          마이페이지
+        </p>
+        {/* 프로필 카드 컴포넌트 (내용은 임시) */}
+        <ProfileCard
+          username={username ? username : "Guest"}
+          userId={email ? email : "itshow@e-mirim.hs.kr"}
+        />
+      </div>
 
-    console.log(token)
-    return (
-        <div className={styles.allContainer}>
-            <div className={styles.profileContainer}>
-                <p className={`${styles.contentName} ${styles.mypageTitle}`}>마이페이지</p>
-                {/* 프로필 카드 컴포넌트 (내용은 임시) */}
-                <ProfileCard
-                    username={username ? username : "Guest"}
-                    userId={email ? email : "itshow@e-mirim.hs.kr"}
-                />
-            </div>
-
-            {/* 실수령액 계산기 */}
-            <Calculator />
-
-        </div>
-    )
+      {/* 실수령액 계산기 */}
+      <Calculator />
+    </div>
+  );
 }
