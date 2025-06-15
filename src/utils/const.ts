6403aaaadd498d76ec31dfd056207e08f8d55f00
juyeonBae <w2316@e-mirim.hs.kr>
2025-06-16 04:03:59 +0900

    feat : 상수 모음(요일, 색상)

---
+++ src/utils/const.ts
@@ -0,0 +1,19 @@
+export const WEEK_DAY_LIST = [
+  "일",
+  "월",
+  "화",
+  "수",
+  "목",
+  "금",
+  "토",
+] as const;
+
+export const CALENDER_FULL_DAY_LENGTH = 42;
+
+export const CALENDER_THEME = {
+  EMPTY: "",
+  ACTIVE_ITEM_TEXT_COLOR: "white",
+  IS_OVER_TIME: "#1A1E6C",
+  HAS_CHECKLIST: "#3B44E6",
+  ACTIVE: "#c8c8c8", // 선택된 날짜
+} as const;
