ce2e72d3b82d1733fe761e6bc049aee94052da52
juyeonBae <w2316@e-mirim.hs.kr>
2025-06-16 04:06:16 +0900

    refactor : 캘린더 구조 변경

--- src/app/components/calender/calender-layout.tsx
+++ src/app/components/calender/calender-layout.tsx
@@ -1,49 +1,59 @@
 "use client";
-import React, { useState } from "react";
-import dayjs from "dayjs";
+import React, { Dispatch, SetStateAction } from "react";
 import Image from "next/image";
 import styles from "../../styles/Calender/calender.module.css";
+import { CalenderDayItemType, generateCalendarDays } from "../../../utils/util";
+import { CALENDER_THEME, WEEK_DAY_LIST } from "../../../utils/const";
 
 type CalendarProps = {
-  setSelectedDate: (date: dayjs.Dayjs) => void;
-  selectedDate: dayjs.Dayjs;
-  checkDates: { month: number; day: number }[];
+  currentYear: number;
+  currentMonth: number;
+  activeDay: number;
+  checkedDayList: number[];
+  overTimeDayList: number[];
+  setCurrentMonth: Dispatch<SetStateAction<number>>;
+  setActiveDay: Dispatch<SetStateAction<number>>;
 };
 
 const Calendar = ({
-  setSelectedDate,
-  selectedDate,
-  checkDates,
+  currentYear,
+  currentMonth,
+  activeDay,
+  checkedDayList,
+  overTimeDayList,
+  setCurrentMonth,
+  setActiveDay,
 }: CalendarProps) => {
-  const [currentDate, setCurrentDate] = useState(dayjs());
-  const startOfMonth = currentDate.startOf("month");
-  const endOfMonth = currentDate.endOf("month");
-  const startDate = startOfMonth.startOf("week");
-  const endDate = endOfMonth.endOf("week");
+  const calenderDays = generateCalendarDays({
+    year: currentYear,
+    month: currentMonth,
+    checkedDayList,
+    overTimeDayList,
+  });
 
-  const weeks = [];
-  let date = startDate;
-  while (date.isBefore(endDate, "day")) {
-    const week = [];
-    for (let i = 0; i < 7; i++) {
-      week.push(date);
-      date = date.add(1, "day");
+  const onClickDayItem = (item: CalenderDayItemType) => {
+    if (item.type === "nextMonth") {
+      setCurrentMonth((prev) => prev + 1);
     }
-    weeks.push(week);
-  }
+    setActiveDay(item.day);
+  };
 
-  const goToPrevMonth = () => setCurrentDate(currentDate.subtract(1, "month"));
-  const goToNextMonth = () => setCurrentDate(currentDate.add(1, "month"));
+  const onClickMonthNavigator = (type: "prevMonth" | "nextMonth") => {
+    setActiveDay(0);
+    setCurrentMonth((prev) => (type === "prevMonth" ? prev - 1 : prev + 1));
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.calenderContainer}>
         <div className={styles.header}>
-          <h2 className={styles.title}>{currentDate.format("YYYY년 MM월")}</h2>
+          <h2
+            className={styles.title}
+          >{`${currentYear}년 ${currentMonth}월`}</h2>
           <div className={styles.prevNextBtnContainer}>
             <Image
               src={"/images/calender-prev-btn.png"}
-              onClick={goToPrevMonth}
+              onClick={() => onClickMonthNavigator("prevMonth")}
               className={styles.button}
               alt="prev"
               width={10}
@@ -51,7 +61,7 @@ const Calendar = ({
             />
             <Image
               src={"/images/calender-next-btn.png"}
-              onClick={goToNextMonth}
+              onClick={() => onClickMonthNavigator("nextMonth")}
               className={styles.button}
               alt="next"
               width={10}
@@ -62,43 +72,46 @@ const Calendar = ({
 
         <div className={styles.calenderContent}>
           <div className={styles.weekdays}>
-            {["일", "월", "화", "수", "목", "금", "토"].map((day) => (
-              <div key={day} className={styles.dayText}>
-                {day}
+            {WEEK_DAY_LIST.map((weekDay) => (
+              <div key={weekDay} className={styles.dayText}>
+                {weekDay}
               </div>
             ))}
           </div>
 
           <div className={styles.days}>
-            {weeks.map((week, wi) =>
-              week.map((day, di) => {
-                const isOtherMonth = !day.isSame(currentDate, "month");
-                const isSelected = selectedDate.isSame(day, "day");
-                console.log("확인:", checkDates, day.month() + 1, day.date());
+            {calenderDays.map((item, index) => {
+              const isNextMonthDayItem = item.type === "nextMonth";
+              const isActive = !isNextMonthDayItem && item.day === activeDay;
 
-                const hasCheckList = checkDates.some(
-                  (d) => day.month() + 1 === d.month && day.date() === d.day
-                );
+              const style = (function () {
+                if (isActive) return { backgroundColor: CALENDER_THEME.ACTIVE };
+                if (item.isOverTime)
+                  return { backgroundColor: CALENDER_THEME.IS_OVER_TIME };
+                if (item.hasCheckList)
+                  return { backgroundColor: CALENDER_THEME.HAS_CHECKLIST };
 
-                let backgroundColor = "";
-                if (hasCheckList) backgroundColor = "#007bff";
-                if (isSelected) backgroundColor = "#ff7f50";
+                return { backgroundColor: CALENDER_THEME.EMPTY };
+              })();
 
-                let className = `${styles.day}`;
-                if (isOtherMonth) className += ` ${styles.other}`;
-
-                return (
-                  <div
-                    key={`${wi}-${di}`}
-                    onClick={() => setSelectedDate(day)}
-                    className={className}
-                    style={{ backgroundColor: backgroundColor }}
-                  >
-                    {day.date()}
-                  </div>
-                );
-              })
-            )}
+              return (
+                <div
+                  key={`calender-day-item-${index}`}
+                  onClick={() => onClickDayItem(item)}
+                  className={`${styles.day} ${
+                    isNextMonthDayItem ? styles.other : ""
+                  }`}
+                  style={{
+                    backgroundColor: style.backgroundColor,
+                    color: style.backgroundColor
+                      ? CALENDER_THEME.ACTIVE_ITEM_TEXT_COLOR
+                      : "",
+                  }}
+                >
+                  {item.day}
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
